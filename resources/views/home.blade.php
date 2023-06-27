05fd036c92f791510743a0d4ddbb442b7f214796
jimbolol12 <97526093+jimbolol12@users.noreply.github.com>
2023-06-27 14:26:43 +0200

    add carts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="headTextEventList">
+    <h1>C punten opdrachten </h1>
+</div>
+<div class="row card-margin">
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="card bg mb-3 mx-auto" style="width: 17rem;">
+        <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title text-center"></h5>
+        <p class="card-text text-center"></p>
+        <a href="#" class="btn btn-dark btn-border btn-send col-md-12 text-center">See this event</a>
         </div>
     </div>
+  
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
