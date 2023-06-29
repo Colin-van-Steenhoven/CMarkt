6bcb5545731f06fc70c1b60a6bf722c2f5b6502b
jimbolol12 <97526093+jimbolol12@users.noreply.github.com>
2023-06-29 15:50:55 +0200

    fixed create detail page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -10,6 +10,8 @@
     </div>
 @endif
 
+<style></style>
+
 <form method="POST" action="{{ route('create-task') }} " enctype="multipart/form-data">
   @csrf
     <div class="contact mt-2">
@@ -27,16 +29,16 @@
                         <input id="points" type="number" name="points" class="form-control">
                     </div>
                   </div>
-                  <div class="col-md-6">
+                  <div class="col-md-6 h-15">
                     <div class="form-group">
                         <label for="places">Aantal plaatsen</label>
-                        <input id="places" type="number" name="places" class="form-control">
+                        <input id="places" type="number" name="places"  class="form-control">
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
                         <label for="description">Beschrijving</label>
-                        <textarea id="description" type="description" name="description" class="form-control"></textarea>
+                        <textarea id="description" type="description" name="description" rows="5" class="form-control resize-vertical"></textarea>
                     </div>
                 </div>
                 <div class="col-md-6">
