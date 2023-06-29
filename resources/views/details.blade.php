6c0ccc7422095782f0ddc08e03959fa8942fe6d2
Colin-van-Steenhoven <93527036+Colin-van-Steenhoven@users.noreply.github.com>
2023-06-29 15:26:03 +0200

    Image op detail geen padding meer en form wel

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -5,8 +5,8 @@
     <div class="row">
         <div class="col-12">
             <div class="position-relative bg-dark text-white">
-                <img src="{{ url('public/Image/'.$tasks->image) }}" class="img-fluid w-100 d-flex align-items-center" alt="Background Image" style="max-height: 300px; object-fit: cover;">
-                <h2 class="position-absolute bg-dark mt- top-50 start-50 mt-1 translate-middle text-center fw-bold">
+                <img id="detailImage" src="{{ url('public/Image/'.$tasks->image) }}" class="img-fluid w-100 d-flex align-items-center" alt="Background Image" style="max-height: 300px; object-fit: cover;">
+                <h2 class="position-absolute bg-dark mt-1 top-50 start-50 translate-middle text-center fw-bold">
                     {{ $tasks->titel }}
                 </h2>
             </div>
