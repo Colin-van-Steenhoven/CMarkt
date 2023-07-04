60274cd54f82d3f8fa7301b125259f1a4efe887f
Brent Albers <93827410+BrentAlbers@users.noreply.github.com>
2023-07-04 15:58:22 +0200

    push

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -5,8 +5,6 @@
         <div class="alert alert-info" role="alert">
             {{ session()->get('message') }}
         </div>
-
-
 @elseif($tasks->users->contains(Auth::user()))
 <div class="alert alert-info bg-success" role="alert">
     <p class="text-light">Je staat al ingeschreven</p>
