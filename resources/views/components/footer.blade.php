9252f50e8fec1394ba3581a05d640b631533fdde
Brent Albers <93827410+BrentAlbers@users.noreply.github.com>
2023-07-04 12:39:18 +0200

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,97 +1,99 @@
-<footer class="footer bg-danger">
-    <div class="footer__addr">
-        <img src="img/Curio-Logo.png" alt="" class="footer__logo">
-        <h2>Contact</h2>
-        <address>
-            Terheijdenseweg 350, 4826 AA Breda<br>
-            <a class="footer__btn" href="mailto:infopunt@curio.nl">Email Ons</a>
-            <a class="footer__btn" href="tel:088-2098000">Bel Ons</a>
-        </address>
+<div class="">
+    <footer class="footer">
+        <div class="footer__addr">
+            <img src="img/Curio-Logo.png" alt="" class="footer__logo">
+            <h2>Contact</h2>
+            <address>
+                Terheijdenseweg 350, 4826 AA Breda<br>
+                <a class="footer__btn" href="mailto:infopunt@curio.nl">Email Ons</a>
+                <a class="footer__btn" href="tel:088-2098000">Bel Ons</a>
+            </address>
+        </div>
+        <li class="nav__item1">
+            <ul class="nav__ul">
+                <li>
+                    <h2>MBO</h2>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/mbo/">Opleidingen</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/mbo/over-het-mbo/">Over het mbo</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/mbo/kies-jouw-mbo/">Open dagen mbo</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/mbo/studie-info/">Studie info</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/mbo/na-het-mbo/">Na het mbo</a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav__item2">
+            <ul class="nav__ul">
+                <li>
+                    <h2>Over Curio</h2>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/nieuws/">Nieuws</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/over-ons/organisatie/">Organisatie</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/over-ons/vakantie/">Vakantie</a>
+                </li>
+                <li>
+                    <a href="https://www.werkenbijcurio.nl/">Vacatures</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/locaties-curio/">Locaties</a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav__item3">
+            <ul class="nav__ul">
+                <li>
+                    <h2>Hulp nodig?</h2>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/hulp-nodig/service-en-contact/">Service en contact</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/faq/">Veelgestelde vragen</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/hulp-nodig/studiekeuze-advies/">Studiekeuze advies</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/hulp-nodig/tips-of-klachten/">Tips of klachten</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/hulp-nodig/inloggen/">Inloggen</a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav__item4">
+            <ul class="nav__ul">
+                <li>
+                    <a href="https://www.curio.nl/cookies/">Cookies</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/privacy/">Privacy</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/disclaimer/">Disclaimer</a>
+                </li>
+                <li>
+                    <a href="https://www.curio.nl/over-ons/international-english/">English</a>
+                </li>
+            </ul>
+        </li>
+    </footer>
+    <div class="legal">
+        <p>&copy; 2022 Curio. All rights reserved.</p>
     </div>
-    <li class="nav__item1">
-        <ul class="nav__ul">
-            <li>
-                <h2>MBO</h2>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/mbo/">Opleidingen</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/mbo/over-het-mbo/">Over het mbo</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/mbo/kies-jouw-mbo/">Open dagen mbo</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/mbo/studie-info/">Studie info</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/mbo/na-het-mbo/">Na het mbo</a>
-            </li>
-        </ul>
-    </li>
-    <li class="nav__item2">
-        <ul class="nav__ul">
-            <li>
-                <h2>Over Curio</h2>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/nieuws/">Nieuws</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/over-ons/organisatie/">Organisatie</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/over-ons/vakantie/">Vakantie</a>
-            </li>
-            <li>
-                <a href="https://www.werkenbijcurio.nl/">Vacatures</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/locaties-curio/">Locaties</a>
-            </li>
-        </ul>
-    </li>
-    <li class="nav__item3">
-        <ul class="nav__ul">
-            <li>
-                <h2>Hulp nodig?</h2>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/hulp-nodig/service-en-contact/">Service en contact</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/faq/">Veelgestelde vragen</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/hulp-nodig/studiekeuze-advies/">Studiekeuze advies</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/hulp-nodig/tips-of-klachten/">Tips of klachten</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/hulp-nodig/inloggen/">Inloggen</a>
-            </li>
-        </ul>
-    </li>
-    <li class="nav__item4">
-        <ul class="nav__ul">
-            <li>
-                <a href="https://www.curio.nl/cookies/">Cookies</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/privacy/">Privacy</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/disclaimer/">Disclaimer</a>
-            </li>
-            <li>
-                <a href="https://www.curio.nl/over-ons/international-english/">English</a>
-            </li>
-        </ul>
-    </li>
-</footer>
-<div class="legal">
-    <p>&copy; 2022 Curio. All rights reserved.</p>
-</div>
+    </div>
 </div>
